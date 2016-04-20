7e8940d90ef57a7ea225633efaa70ef2007ca4a2
Julen Landa Alustiza <julen@zokormazo.info>
2016-04-20 18:01:15 +0200

    typo fix, replace bot nick with sender's nick, add cobe.dbs dir and requirements.txt

--- bot.py
+++ bot.py
@@ -10,13 +10,14 @@ config = ConfigParser.ConfigParser()
 config.read("bot.cfg")
 
 def handle(msg):
-    print msg
     content_type, chat_type, chat_id = telepot.glance(msg)
     if content_type == 'text':
         brain = Brain(config.get('Brain', 'path') + str(chat_id) + ".brain")
         brain.learn(msg['text'])
-        if 'braulio' in msg['text'].lower():
-            bot.sendMessage(chat_id,brain.reply(msg['text']))
+        if 'reply_to_message' in msg and msg['reply_to_message']['from']['username'] == "Braulio_bot":
+            bot.sendMessage(chat_id,brain.reply(msg['text']),reply_to_message_id=msg['message_id'])
+        elif 'braulio' in msg['text'].lower():
+            bot.sendMessage(chat_id,brain.reply(msg['text']).replace("Braulio",msg['from']['first_name']))
 
 token = config.get('General', 'token')
 bot = telepot.Bot(token)
